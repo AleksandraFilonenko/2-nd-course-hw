f1706e60da6e4d0255b689bd66749ffd6ea0d804
Александра Владимировна <aleksa_filonenko@mail.ru>
2026-03-06 16:47:15 +0300

    hw-2 js

---
+++ hw-2/script.js
@@ -0,0 +1,58 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 2
+let year = 2007;
+alert(year);
+
+// Задание 3
+let creatorName = "Брендан Эйх";
+alert("Создатель JavaScript - " + creatorName);
+
+// Задание 4
+let b = 10;
+let c = 2;
+let sum = b + c;
+let difference = b - c;
+let product = b * c;
+let quotient = b / c;
+alert("Сумма: " + sum + ", разность: " + difference + ", произведение: " + product + ", частное: " + quotient);
+
+// Задание 5
+let numb = 2 ** 5;
+let result = numb;
+alert(result);
+
+// Задание 6
+let aa = 9;
+let bb = 2;
+let abResult = aa % bb;
+alert(abResult);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// Задание 9
+const user = {
+    userName: 'Alex',
+    age: 21,
+    isAdmin: true
+}; 
+
+// Задание 10
+let personName = prompt("Как вас зовут?");
+alert("Привет, " + personName + "!")
